aa350176e7f7fff1b8cb58b3ec118323e3d3060d
leonel <leonel.mariano.g@gmail.com>
2026-01-13 18:47:24 -0300

    ref(kanban): Extract data into its own file

---
+++ kanban-board/data/db.js
@@ -0,0 +1,194 @@
+export default [
+    {
+        id: 1,
+        title: 'To Do',
+        cards: [
+            {
+                id: 1,
+                content: 'Add Authentication',
+                labels: [
+                    {
+                        name: 'Frontend',
+                        color: '#00b026',
+                    },
+                    {
+                        name: 'Backend',
+                        color: '#B80EFF',
+                    }
+                ]
+            },
+            {
+                id: 1,
+                content: 'Add Authentication',
+                labels: [
+                    {
+                        name: 'Frontend',
+                        color: '#00b026',
+                    },
+                    {
+                        name: 'Backend',
+                        color: '#B80EFF',
+                    }
+                ]
+            },
+            {
+                id: 1,
+                content: 'Add Authentication',
+                labels: [
+                    {
+                        name: 'Frontend',
+                        color: '#00b026',
+                    },
+                    {
+                        name: 'Backend',
+                        color: '#B80EFF',
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: 'In Progress',
+        cards: [
+            {
+                id: 1,
+                content: 'Add Authentication',
+                labels: [
+                    {
+                        name: 'Frontend',
+                        color: '#00b026',
+                    },
+                    {
+                        name: 'Backend',
+                        color: '#B80EFF',
+                    }
+                ]
+            },
+            {
+                id: 1,
+                content: 'Add Authentication',
+                labels: [
+                    {
+                        name: 'Frontend',
+                        color: '#00b026',
+                    },
+                    {
+                        name: 'Backend',
+                        color: '#B80EFF',
+                    }
+                ]
+            },
+            {
+                id: 1,
+                content: 'Add Authentication',
+                labels: [
+                    {
+                        name: 'Frontend',
+                        color: '#00b026',
+                    },
+                    {
+                        name: 'Backend',
+                        color: '#B80EFF',
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        id: 3,
+        title: 'Review',
+        cards: [
+            {
+                id: 1,
+                content: 'Add Authentication',
+                labels: [
+                    {
+                        name: 'Frontend',
+                        color: '#00b026',
+                    },
+                    {
+                        name: 'Backend',
+                        color: '#B80EFF',
+                    }
+                ]
+            },
+            {
+                id: 1,
+                content: 'Add Authentication',
+                labels: [
+                    {
+                        name: 'Frontend',
+                        color: '#00b026',
+                    },
+                    {
+                        name: 'Backend',
+                        color: '#B80EFF',
+                    }
+                ]
+            },
+            {
+                id: 1,
+                content: 'Add Authentication',
+                labels: [
+                    {
+                        name: 'Frontend',
+                        color: '#00b026',
+                    },
+                    {
+                        name: 'Backend',
+                        color: '#B80EFF',
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: 'Done',
+        cards: [
+            {
+                id: 1,
+                content: 'Add Authentication',
+                labels: [
+                    {
+                        name: 'Frontend',
+                        color: '#00b026',
+                    },
+                    {
+                        name: 'Backend',
+                        color: '#B80EFF',
+                    }
+                ]
+            },
+            {
+                id: 1,
+                content: 'Add Authentication',
+                labels: [
+                    {
+                        name: 'Frontend',
+                        color: '#00b026',
+                    },
+                    {
+                        name: 'Backend',
+                        color: '#B80EFF',
+                    }
+                ]
+            },
+            {
+                id: 1,
+                content: 'Add Authentication',
+                labels: [
+                    {
+                        name: 'Frontend',
+                        color: '#00b026',
+                    },
+                    {
+                        name: 'Backend',
+                        color: '#B80EFF',
+                    }
+                ]
+            },
+        ]
+    }
+]
