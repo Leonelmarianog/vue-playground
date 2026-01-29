a7582c820b4c8a43ae7c69a90f24e112e460acd2
leonel <leonel.mariano.g@gmail.com>
2026-01-29 13:45:53 -0300

    Add padding options to CustomButton

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,15 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 
-const BASE_STYLE = 'cursor-pointer disabled:cursor-not-allowed rounded-sm py-2 px-6 text-nowrap';
-
-const VARIANT_OPTIONS = {
-  default: 'bg-neutral-600 hover:bg-neutral-700 disabled:bg-neutral-300 text-white',
-  green: 'bg-green-600 hover:bg-green-700 disabled:bg-green-300 text-white',
-  transparent: 'bg-black/60 hover:bg-black/80 disabled:bg-black/30 text-white',
-  clear: 'bg-transparent hover:bg-gray-100 disabled:text-gray-400 text-gray-800',
-};
-
 const emit = defineEmits<{
   (e: 'click'): void;
 }>();
@@ -18,17 +9,36 @@ const props = withDefaults(
   defineProps<{
     variant?: 'default' | 'green' | 'transparent' | 'clear';
     fullWidth?: boolean;
+    padding?: 'xs' | 'sm' | 'md' | 'lg';
   }>(),
   {
     variant: 'default',
     fullWidth: false,
+    padding: 'md',
   },
 );
 
+const BASE_STYLE = 'cursor-pointer disabled:cursor-not-allowed rounded-sm text-nowrap';
+
+const VARIANT_OPTIONS = {
+  default: 'bg-neutral-600 hover:bg-neutral-700 disabled:bg-neutral-300 text-white',
+  green: 'bg-green-600 hover:bg-green-700 disabled:bg-green-300 text-white',
+  transparent: 'bg-black/60 hover:bg-black/80 disabled:bg-black/30 text-white',
+  clear: 'bg-transparent hover:bg-gray-100 disabled:text-gray-400 text-gray-800',
+};
+
+const PADDING_OPTIONS = {
+  xs: 'py-1 px-3 text-sm',
+  sm: 'py-1.5 px-4 text-sm',
+  md: 'py-2 px-6',
+  lg: 'py-3 px-8 text-lg',
+};
+
 const classes = computed(() => {
-  const variantClass = VARIANT_OPTIONS[props.variant];
+  const variant = VARIANT_OPTIONS[props.variant];
+  const padding = PADDING_OPTIONS[props.padding];
   const width = props.fullWidth ? 'w-full' : '';
-  return [BASE_STYLE, width, variantClass];
+  return [BASE_STYLE, variant, padding, width];
 });
 
 const onClick = () => {
