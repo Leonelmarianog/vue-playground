c7cf83c719c413ab86fe01f0f58d05a45d589a4d
leonel <leonel.mariano.g@gmail.com>
2026-01-29 23:14:08 -0300

    Move list store to BoardContainer

--- src/components/BoardContainer.vue
+++ src/components/BoardContainer.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 import CustomButton from '@/components/CustomButton.vue';
 import { Plus } from 'lucide-vue-next';
+import BoardList from '@/components/BoardList.vue';
+import { useListStore } from '@/stores/list.ts';
+
+const listStore = useListStore();
 </script>
 
 <template>
   <div id="board" class="grid auto-cols-[20em] grid-flow-col gap-2 items-start h-full">
-    <slot name="default" />
+    <ul>
+      <li v-for="list in listStore.lists" :key="list.id">
+        <BoardList :list="list" />
+      </li>
+    </ul>
 
     <CustomButton variant="transparent">
       <span class="flex justify-center items-center gap-1">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,16 @@
 <script setup lang="ts">
-import BoardList from '@/components/BoardList.vue';
-import PageLayout from '@/components/PageLayout.vue';
 import BoardContainer from '@/components/BoardContainer.vue';
-import { useListStore } from '@/stores/list';
-
-const listStore = useListStore();
+import PageLayout from '@/components/PageLayout.vue';
 </script>
 
 <template>
   <PageLayout>
-    <template v-slot:heading>
+    <template #heading>
       <h1 class="font-bold text-3xl text-center">My board</h1>
     </template>
 
-    <template v-slot:default>
-      <BoardContainer>
-        <template v-slot:default>
-          <BoardList v-for="list in listStore.lists" :key="list.id" :list="list" />
-        </template>
-      </BoardContainer>
+    <template #default>
+      <BoardContainer />
     </template>
   </PageLayout>
 </template>
