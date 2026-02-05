b00ca8c9efab0f3d812dc73b3562c3ba3b41df38
leonel <leonel.mariano.g@gmail.com>
2026-02-05 20:03:55 -0300

    Register initial API level routes

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -6,6 +6,10 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        api: [
+            __DIR__.'/../routes/api.php',
+            __DIR__.'/../routes/api_v1.php'
+        ],
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
