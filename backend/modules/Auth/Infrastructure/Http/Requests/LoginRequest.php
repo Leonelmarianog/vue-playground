932b57314bbe0a62e58df1010617fafe574175a8
leonel <leonel.mariano.g@gmail.com>
2026-02-18 01:12:55 -0300

    chore(backend): rollback change of class being final

--- backend/modules/Auth/Infrastructure/Http/Requests/LoginRequest.php
+++ backend/modules/Auth/Infrastructure/Http/Requests/LoginRequest.php
@@ -5,7 +5,7 @@ namespace Modules\Auth\Infrastructure\Http\Requests;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-final class LoginRequest extends FormRequest
+class LoginRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
